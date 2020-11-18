725f6b671cebeb97756d7ce2891f35364ea79e37
rajnu <fanir@cip.ifi.lmu.de>
2020-11-18 01:47:32 +0100

    :(

--- src/hw02_objects/oop_1_bank.py
+++ src/hw02_objects/oop_1_bank.py
@@ -3,7 +3,7 @@
 a) Using the slides & the script, put together a file containing the
    complete Account class.  Each method must have a documentation
    string at the beginning which describes what the method is doing.
-   (1 point) TODO
+   (1 point)
 
 b) Create a main application where you create a number of accounts.
    Play around with depositing / withdrawing money.  Change the
@@ -22,21 +22,56 @@ e) Implement the __str__ magic method and use it to print accounts.
 import re
 
 
-class Account:
-    """ Here has to be a documentation string that describes
-    which data objects this class is designed for.
-    You have to remove the pass statement and then write some
-    code for the class. """
+class Account(object):
+    """ This is a class that represents a bank account
+    Attributes
+    ----------
+    num: int
+         personal number of the person
+    holder : String
+             the name of the person
+
+    Methods and functions
+    ------
+    deposit(self, amount): this method is used to show the balance after depositing cash to the bank account
+    withdraw(self, amount): shows the amount that is about to be withdrawn
+    set_holder(self, person): the setter method helps changing the account holder of an account
+    get_holder(self): the getter method is used to complete the change made with the setter method
+    __str__(self): this method is used to print accounts
+    apply_interest(self): this is a function which applies an interest rate of 1.5%
+                        to the current balance and call it on the given objects
+    __str__(self): a method used to print accounts
+
+     """
 
     def __init__(self, num, person):
+        """
+
+
+        :param num: int (the personal number of the person)
+        :param person: string (the name of the person/holder)
+        """
         self.balance = 0
         self.number = num
         self.holder = person
 
     def deposit(self, amount):
+        """
+        Shows the balance after depositing cash to the bank account
+
+        :param amount: int
+        :return: balance :: double
+        """
         self.balance += amount
+        return self.balance
 
     def withdraw(self, amount):
+        """
+        Shows the amount that is about to be withdrawn
+
+        :param amount: double
+        :return: amount :: double
+        """
         if self.balance - amount < -1000:
             amount = self.balance + 1000
 
@@ -44,9 +79,19 @@ class Account:
         return amount
 
     def print_info(self):
+        """
+
+        :return: Prints the balance value as a string
+        """
         print("Balance: ", self.balance)
 
     def set_holder(self, person):
+        """
+        the setter method helps changing the account holder of an account
+
+        :param person: str : The name of the person
+        :return: str
+        """
         if (not type(person) == str):
             raise TypeError
         if not re.match("\w+( \w+)*", person.strip()):
@@ -54,12 +99,25 @@ class Account:
         self.holder = person
 
     def get_holder(self):
+        """
+        The getter method is used to help the setter method
+        """
         return self.holder
 
     def apply_interest(self):
+        """
+        A function which applies an interest
+        rate of 1.5% to the current balance and call it on your objects.
+        :return: double
+        """
         self.balance *= 1.015
 
     def __str__(self):
+        """
+        a method used to print accounts
+
+        :return: str
+        """
         res = "***Account Info***\n Account ID: %i \n Holder: %s \n Balance: %i" % (
             self.number, self.holder, self.balance)
 
@@ -68,3 +126,4 @@ class Account:
 
 if __name__ == "__main__":
     print("Welcome to the Python Bank!")
+
