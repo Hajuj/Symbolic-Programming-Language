a88ffa2bfe02dbf157a2ed4513cb32321cbc160a
Hajuj <hgogm@cip.ifi.lmu.de>
2020-11-26 00:27:41 +0100

    privateee

--- src/hw03_documents/document.py
+++ src/hw03_documents/document.py
@@ -1,47 +1,52 @@
-import nltk
+from nltk import word_tokenize
 
-def word_tokenize(text):
-    return nltk.word_tokenize(text)
+
+def word(text):
+    return word_tokenize(text)
+
 
 def normalized_tokens(text):
     """ This takes a string and returns lower-case tokens, using nltk for tokenization. """
-    tokens = word_tokenize(text.toLower())
-    return tokens
+    return word_tokenize(text.lower())
 
-def wordCount(text):
-    ourText = word_tokenize(text)
-    sth = {}
-    for word in ourText:
-        if word in sth:
-            sth[word] += 1
+
+def count(text):
+    text = word_tokenize(text)
+    d = dict()
+    for word in text:
+        if word in d:
+            d[word] += 1
         else:
-            sth[word] = 0
-    return sth
+            d[word] = 1
+        for key in list(d.keys()):
+            print(key + ":", d[key])
+    return d
+
 
 class TextDocument:
-    def __init__(self, text, id=None):
+    def __init__(self, text, id):
         """ This creates a TextDocument instance with a string, a dictionary and an identifier. """
         self.text = text
-        self.word_to_count = wordCount(text)
+        self.word_to_count = count(text)
         self.id = id
 
     @classmethod
     def from_file(cls, filename):
         """ This creates a TextDocument instance by reading a file. """
-        text = "" # TODO: read text from filename
+        text = ""  # TODO: read text from filename
         return cls(text, filename)
 
     def __str__(self):
         """ This returns a short string representation, which is at most 25 characters long.
         If the original text is longer than 25 characters, the last 3 characters of the short string should be '...'.
         """
-
-        result = ' '
-        pass # TODO: Implement correct return statement.
+        if (text.length > 25):
+            string = string.substring(0, 24) + "..."
+        pass  # TODO: Implement correct return statement.
 
     def word_overlap(self, other_doc):
         """ This returns the number of words that occur in both documents (self and other_doc) at the same time.
         Every word should be considered only once, irrespective of how often it occurs in either document (i.e. we
         consider word *types*).
         """
-        pass # TODO: Implement correct return statement.
+        pass  # TODO: Implement correct return statement.
